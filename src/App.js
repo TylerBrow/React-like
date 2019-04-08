12528f550780efd3353a8663a1435c33ff792f33
Tyler Brow <tymuska@yahoo.com>
2019-04-08 16:01:21 -0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
 
 class App extends Component {
+  state = {
+    countLikes : 0,
+    str : 'Likes'
+  }
+
+  countLikes = () => {
+    this.setState ({
+    countLikes : this.state.countLikes + 1
+  })
+    if (this.state.countLikes === 0){
+      this.setState ({
+        str : 'Like'
+      })
+    } else {
+      this.setState ({
+        str: 'Likes'
+      })
+    }
+}
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button onClick = {this.countLikes}>{this.state.countLikes} {this.state.str}</button>
       </div>
     );
   }
